dda46a1c4474a86cc634dc600d4df6ef930774aa
Willem Hueffed <willmhueffed@gmail.com>
2024-03-09 17:40:42 -0800

    Initial commit

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,85 @@
-fn main() {
-    println!("Hello, world!");
+extern crate sdl2;
+use sdl2::event::Event;
+use sdl2::image::{self, InitFlag, LoadTexture};
+use sdl2::keyboard::Keycode;
+use sdl2::pixels::Color;
+use sdl2::rect::{Point, Rect};
+use sdl2::render::{Texture, WindowCanvas};
+use std::time::Duration;
+
+#[derive(Debug)]
+struct Player {
+    position: Point,
+    sprite: Rect,
+}
+
+fn render(
+    canvas: &mut WindowCanvas,
+    color: Color,
+    texture: &Texture,
+    player: &Player,
+) -> Result<(), String> {
+    canvas.set_draw_color(color);
+    canvas.clear();
+    let (width, height) = canvas.output_size()?;
+
+    //Treat the center of the screen as (0,0)
+    let screen_position = player.position + Point::new(width as i32 / 2, height as i32 / 2);
+    let screen_rect = Rect::from_center(
+        screen_position,
+        player.sprite.width(),
+        player.sprite.height(),
+    );
+
+    canvas.copy(texture, player.sprite, screen_rect)?;
+    canvas.present();
+
+    Ok(())
+}
+
+fn main() -> Result<(), String> {
+    let sdl_context = sdl2::init()?;
+    let video_subsystem = sdl_context.video()?;
+
+    let _image_context = image::init(InitFlag::PNG | InitFlag::JPG)?;
+
+    let window = video_subsystem
+        .window("game tutorial", 800, 600)
+        .position_centered()
+        .build()
+        .expect("could not initialize the video subsystem");
+
+    let mut canvas = window
+        .into_canvas()
+        .build()
+        .expect("could not make a canvas");
+
+    let texture_creator = canvas.texture_creator();
+    let texture = texture_creator.load_texture("assets/bardo.png")?;
+
+    let player = Player {
+        position: Point::new(0, 0),
+        sprite: Rect::new(0, 0, 26, 36),
+    };
+
+    let mut event_pump = sdl_context.event_pump()?;
+    let mut i = 0;
+    'running: loop {
+        for event in event_pump.poll_iter() {
+            match event {
+                Event::Quit { .. }
+                | Event::KeyDown {
+                    keycode: Some(Keycode::Escape),
+                    ..
+                } => break 'running,
+                _ => {}
+            }
+        }
+
+        i = (i + 1) % 255;
+        render(&mut canvas, Color::RGB(i, 64, 255 - i), &texture, &player)?;
+
+        ::std::thread::sleep(Duration::new(0, 1_000_000_000u32 / 60));
+    }
+    Ok(())
 }
